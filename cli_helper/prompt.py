c21d4179aefc1a58a71de3be8c1bd8e92e9eb339
Caspar Trittibach <caspar.trittibach@haag-streit.com>
2024-09-04 15:16:54 +0200

    work

--- cli_helper/prompt.py
+++ cli_helper/prompt.py
@@ -1,12 +1,13 @@
 import os
 import inquirer
 
-def nested_prompt(data: dict, parent: any = None) -> any:
+def nested_prompt(data: dict, parent: any = None, level = 0) -> any:
     ret = data
     selection = []
+    level += 1
     while type(ret) == dict:
         if parent:
-            selection.append(("<-- back", parent))
+            selection.append(("<-- back", None))
         for key, value in data.items():
             if type(value) is dict:
                 sel = ("--> " + key, value)
@@ -21,7 +22,7 @@ def nested_prompt(data: dict, parent: any = None) -> any:
         ]
         answer = inquirer.prompt(question)
         #ret = answer["template"]
-        ret = nested_prompt(answer["template"], data)
+        ret = nested_prompt(answer["template"], data, level)
         #print(ret)
         #if type(ret) is dict:
         #    ret = nested_prompt(ret)
