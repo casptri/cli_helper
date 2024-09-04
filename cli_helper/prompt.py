3dfd7d59e7ff10da7ce356657e4e623447f17805
Caspar Trittibach <caspar.trittibach@haag-streit.com>
2024-09-04 14:37:59 +0200

    work

--- cli_helper/prompt.py
+++ cli_helper/prompt.py
@@ -2,21 +2,23 @@ import os
 import inquirer
 
 def nested_prompt(data: dict) -> any:
-    selection = [("<-- back", data) ]
-    for key, value in data.items():
-        if type(value) is dict:
-            sel = ("--> " + key, value)
-            selection.append(sel)
-        else:
-            sel = (" * " + key, value)
-            selection.append(sel)
-    question = [
-        inquirer.List(
-            "template", message="Template Menu", choices=selection, carousel=True
-        ),
-    ]
-    answer = inquirer.prompt(question)
-    ret = answer["template"]
-    if type(ret) is dict:
-        sel = nested_prompt(ret)
+    ret = None
+    while not ret:
+        selection = [("<-- back", None) ]
+        for key, value in data.items():
+            if type(value) is dict:
+                sel = ("--> " + key, value)
+                selection.append(sel)
+            else:
+                sel = (" * " + key, value)
+                selection.append(sel)
+        question = [
+            inquirer.List(
+                "template", message="Template Menu", choices=selection, carousel=True
+            ),
+        ]
+        answer = inquirer.prompt(question)
+        ret = answer["template"]
+        if type(ret) is dict:
+            ret = nested_prompt(ret)
     return sel
