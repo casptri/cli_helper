25f9078f71c186e56dc4c1f70ab22061f3f4eecd
Caspar Trittibach <caspar.trittibach@haag-streit.com>
2024-09-04 13:17:09 +0200

    Work on propmt dict

--- cli_helper/prompt.py
+++ cli_helper/prompt.py
@@ -1,2 +1,20 @@
+
 def prompt_dict(data: dict):
-    print("get prompt from {dict}")
+
+    os.system("clear")
+    selection = []
+    for key, value in data.items():
+        if type(value) == dict:
+            sel = ("--> " + key, key)
+            selection.append(sel)
+        else:
+            sel = (" * " + key, key)
+            selection.append(sel)
+    question = [
+        inquirer.List(
+            "template", message="Template Menu", choices=selection, carousel=True
+        ),
+    ]
+    answer = inquirer.prompt(question)
+    ret = answer["template"]
+    return ret
